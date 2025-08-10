3b122d4c2364438aa512f5eba54806026c45db2b
sure15 <gxishk15@gmail.com>
2025-08-10 22:56:34 +0800

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,17 +10,27 @@ const Button = (props) => {
 
 const Statistics = (props) => {
   const { good, neutral, bad } = props
-  return (
-    <>
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good / (good + neutral + bad) * 100} %</p>
-    </>
-  )
+  if (good > 0 || neutral > 0 || bad > 0) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {(good - bad) / (good + neutral + bad)}</p>
+        <p>positive {good / (good + neutral + bad) * 100} %</p>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
 }
 
 const App = () => {
