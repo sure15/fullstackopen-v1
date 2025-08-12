3333b735791f93bf16c90d39055843f5b751c291
sure15 <gxishk15@gmail.com>
2025-08-13 02:53:14 +0800

    2.7: The Phonebook Step 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,15 +8,20 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName
+
+    if (persons.some(obj => obj.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const nameObject = {
+        name: newName
+      }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
     }
-    setPersons(persons.concat(nameObject))
-    setNewName('')
   }
 
   const handleNameChange = (event) => {
-    console.log('handleNameChange event', event.target)
+    console.log('handleNameChange event', event.target.value)
     setNewName(event.target.value)
   }
 
