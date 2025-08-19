00902a013a1129c4c3f35e67ac93e7e55113e7d6
sure15 <gxishk15@gmail.com>
2025-08-20 04:06:03 +0800

    part3b Application to the Internet

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -88,7 +90,7 @@ const unkownEndpoint = (request, response) => {
 
 app.use(unkownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
