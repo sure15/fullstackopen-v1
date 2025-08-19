1755716028e3b90c2d3b441a1477dd63cf9ae05f
sure15 <gxishk15@gmail.com>
2025-08-20 01:29:40 +0800

    3.7: Phonebook backend step 7

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -3,6 +3,16 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(requestLogger)
+
 let notes = [
     {
         id: "1",
@@ -71,6 +81,12 @@ app.post('/api/notes', (request, response) => {
 
     response.json(note)
 })
+
+const unkownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unkown endpoint' })
+}
+
+app.use(unkownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
