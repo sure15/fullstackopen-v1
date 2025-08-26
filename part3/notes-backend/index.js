2630b813e81385ec1553df191cae1c1ac956ffc3
sure15 <gxishk15@gmail.com>
2025-08-26 21:54:57 +0800

    part3c before excercises 3.15

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -4,8 +4,8 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -46,18 +46,24 @@ app.get('/api/notes', (request, response) => {
 
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
-
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -80,14 +86,21 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response) => {
-    const body = request.body
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
+    const { content, important } = request.body
+
+    Note.findById(request.params.id)
+        .then(note => {
+            if (!note) {
+                return response.status(404).end()
+            }
+            note.content = content
+            note.important = important
+
+            return note.save().then(updatedNote => {
+                response.json(updatedNote)
+            })
         })
-    }
-    notes = notes.filter(note => note.id !== body.id).concat(body)
-    response.json(body)
+        .catch(error => next(error))
 })
 
 const unkownEndpoint = (request, response) => {
@@ -95,6 +108,19 @@ const unkownEndpoint = (request, response) => {
 }
 
 app.use(unkownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
