b8ab281a161ca0f067ba798a0329eb8bac77f4b4
sure15 <gxishk15@gmail.com>
2025-08-26 14:16:55 +0800

    3.12: Command-line database

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
