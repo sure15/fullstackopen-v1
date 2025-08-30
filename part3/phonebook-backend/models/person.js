3f60cbfb72afd31b8c5e995a3f1da4e2e3f63c02
sure15 <gxishk15@gmail.com>
2025-08-30 15:31:41 +0800

    3.20*: Phonebook database, step 8

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
         minLength: 5,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    }
 })
 
 personSchema.set('toJSON', {
