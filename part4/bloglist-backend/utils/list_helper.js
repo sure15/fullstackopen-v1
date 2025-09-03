6b1d204470be1b4c6760e0074fdf9648311b2d55
sure15 <gxishk15@gmail.com>
2025-09-03 19:04:11 +0800

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -9,6 +9,12 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
