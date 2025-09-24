dc913f240feabd7db2bc7a7f3c3d53430e41dc3f
sure15 <gxishk15@gmail.com>
2025-09-25 03:32:16 +0800

    4.15: Blog List Expansion, step 3

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -10,5 +11,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
